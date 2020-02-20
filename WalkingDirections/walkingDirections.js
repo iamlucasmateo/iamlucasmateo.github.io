19e2fb7f8133ca0203a618b7bb00c3cfd5131556
Lucas Mateo Perrone <58492520+iamlucasmateo@users.noreply.github.com>
2020-02-20 10:39:57 -0300

    walking directions first iteration

--- WalkingDirections/walkingDirections.js
+++ WalkingDirections/walkingDirections.js
@@ -1,5 +1,7 @@
 var mapboxAccessToken = "pk.eyJ1IjoibHVjY2hpbmkyMDIiLCJhIjoiY2s0ZjAydWsxMGpldzNkbzdxM2YwM2pzeiJ9.yeXrZv8dt3jP9lZDPpoRVQ";
 var map = L.map('map').setView([-34.610824, -58.451452], 12);
+var go = document.querySelector('#go')
+var pointA = document.querySelector('#pointA')
 
 
 // Fondo Mapbox
@@ -8,9 +10,23 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}{r}.png', {
 }).addTo(map);
 
 
-L.Routing.control({
+function goFunc(latA,lonA,latB,lonB) {
+  L.Routing.control({
     waypoints: [
-      L.latLng(-34.610824, -58.451452),
-      L.latLng(-34.613824, -58.452452)
-    ]
+      L.latLng(latA, lonA),
+      L.latLng(latB, lonB)
+    ],
+    router: L.Routing.mapbox(mapboxAccessToken)
   }).addTo(map);
+}
+
+go.addEventListener('click',function(){
+  let coordA = pointA.value.split(' ')
+  let coordB = pointB.value.split(' ')
+  let latA = parseFloat(coordA[0]);
+  let lonA = parseFloat(coordA[1]);
+  let latB = parseFloat(coordB[0]);
+  let lonB = parseFloat(coordB[1]); 
+  goFunc(latA,lonA,latB,lonB)
+})
+
