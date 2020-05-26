7b67d2f2f1f209c2b6b6e0031f2f1c8a4216ed23
Lucas Mateo Perrone <58492520+iamlucasmateo@users.noreply.github.com>
2020-05-26 13:26:37 -0300

    update glovo

--- Pruebas/glovo.js
+++ Pruebas/glovo.js
@@ -1,1 +1,21 @@
-window.location = "http://www.google.com";
+
+var glovoLink = document.querySelector("#glovo-link") 
+
+var redirect = function() {
+    var app = {
+      launchApp: function() {
+        window.location.replace("glovo://");
+        this.timer = setTimeout(this.openWebApp, 2000);
+      },
+  
+      openWebApp: function() {
+        window.location.replace("https://www.google.com/");
+      }
+    };
+  
+    app.launchApp();
+};
+
+
+glovoLink.addEventListener('click',redirect);
+
