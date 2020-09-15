df78927ca30148fd5ecdfc1d980b5cea05befd2a
iamlucasmateo <lucasmateo300@gmail.com>
2020-09-15 06:48:49 -0300

    added value Event

--- AnalyticsTesting/analyticstesting.js
+++ AnalyticsTesting/analyticstesting.js
@@ -5,6 +5,14 @@ function sendGAEvent(action,category,label){
     });
 };
 
+function sendGAValueEvent(data){
+    gtag('event', data.action, {
+    'event_category': data.category,
+    'event_label': data.label,
+    'value': data.value
+    });
+};
+
 function sendGAClickEvent(element){
     element.addEventListener('click',function(event){
         const action = event.type;
@@ -19,6 +27,34 @@ const title = document.getElementById('title')
 const subtitle = document.getElementById('subtitle')
 sendGAClickEvent(title);
 sendGAClickEvent(subtitle);
+
+// Event with Value
+
+const eventTrigger = document.getElementById('event-trigger'),
+      eventLabel = document.getElementById('event-label'),
+      eventValue = document.getElementById('event-value');
+
+eventTrigger.addEventListener('click',function(event){
+    const data = {};
+    data['action'] = event.type;
+    data['category'] = 'AnalyticsTesting/analytics-testing';
+    data['label'] = eventLabel.value;
+    data['value'] = eventValue.value;
+    sendGAValueEvent(data);
+    console.log('Data sent from trigger');
+})      
+
+
+// function sendTealiumEvent(element,eventType,eventDetails){
+//     element.addEventListener(eventType,function(evt){
+//         utag.link({
+//             event: evenDetails.name,
+//             eCategory: eventDetails.category,
+//             eAction: eventDetails.action,
+//             eLabel: eventDetails.label
+//         });
+//     });
+// };
 
 
 
