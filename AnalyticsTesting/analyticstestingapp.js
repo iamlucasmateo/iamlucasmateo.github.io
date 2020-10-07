663a85c0f33ff80ac5e86dbdf70741d3aa97f5bd
iamlucasmateo <lucasmateo300@gmail.com>
2020-10-07 16:05:04 -0300

    update dataLayer push events

--- AnalyticsTesting/analyticstestingapp.js
+++ AnalyticsTesting/analyticstestingapp.js
@@ -1,1 +1,10 @@
+var dlButtons = document.querySelectorAll('.dlbutton');
+
+dlButtons.forEach(function(button){
+    button.addEventListener('click', function(){
+        dataLayer.push({event: this.innerHTML})
+    });
+})
+
+
 
