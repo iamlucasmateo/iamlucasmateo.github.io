e5985991d8743a927de65b5c1a9f63f460463b1a
iamlucasmateo <lucasmateo300@gmail.com>
2021-03-10 00:51:41 -0300

    create La Irenita

---
+++ WebProjs/LaIrenita/assets/js/home.js
@@ -0,0 +1,34 @@
+const selectPilar = document.getElementById('button-pilar');
+const selectDaireaux = document.getElementById('button-daireaux');
+const mapPilar = document.getElementById('map-pilar');
+const mapDaireaux = document.getElementById('map-daireaux');
+const mailPilar = document.getElementById('mail-pilar');
+const mailDaireaux = document.getElementById('mail-daireaux');
+
+let isShowingPilar = true;
+
+function changeActive(active, inactive, activeClass, inactiveClass) {
+    inactive.classList.remove(activeClass);
+    active.classList.remove(inactiveClass);
+    inactive.classList.add(inactiveClass);
+    active.classList.add(activeClass);
+};
+
+selectPilar.addEventListener('click',() => {
+    if (!isShowingPilar) {
+        changeActive(mapPilar, mapDaireaux, 'mapas__container-active', 'mapas__container-inactive');
+        changeActive(selectPilar, selectDaireaux, 'mapas__button-active', 'mapas__button-inactive');
+        changeActive(mailPilar, mailDaireaux, 'mail-tel__active', 'mail-tel__inactive');
+        isShowingPilar = true;
+    };
+});
+
+selectDaireaux.addEventListener('click',() => {
+    if (isShowingPilar) {
+        changeActive(mapDaireaux, mapPilar, 'mapas__container-active', 'mapas__container-inactive');
+        changeActive(selectDaireaux, selectPilar, 'mapas__button-active', 'mapas__button-inactive');
+        changeActive(mailDaireaux, mailPilar, 'mail-tel__active', 'mail-tel__inactive');
+        isShowingPilar = false;
+    };
+});
+
