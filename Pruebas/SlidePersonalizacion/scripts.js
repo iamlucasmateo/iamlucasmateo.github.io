6f7db375e32a4cbf0161582569d783a3bc0bf7d8
iamlucasmateo <lucasmateo300@gmail.com>
2021-03-26 03:56:41 -0300

    Update scripts.js

--- Pruebas/SlidePersonalizacion/scripts.js
+++ Pruebas/SlidePersonalizacion/scripts.js
@@ -1,8 +1,9 @@
+/*
 // Home
 const liHtml = `<li>
                     <a href=""  class="item" data-gact="" data-gaid="">
                         <div class="imageContainer">
-                            <img src="https://imgflip.com/i/537nof" class="desktop"/>
+                            <img src="http://www.santander.com.ar/banco/wcm/myconnect/28b62b7d-0023-4723-8320-29edadd6db98/drakeSlide.png?MOD=AJPERES" class="desktop"/>
                         </div>
                         <div class="detailContainer">
                             <div class="table">
@@ -16,7 +17,7 @@ $('.flexslider').data('flexslider').addSlide(liHtml,0);
 
 // Login
 document.querySelector('.welcome-container__message').innerHTML = '<p>Hola :), <br> ¿sabías que <br>tenemos <br> esta oferta <br>para vos?';
-document.getElementsByTagName('login')[0].style['background-image'] = 'url("https://www.santander.com.ar/banco/wcm/connect/589cadf5-fcdb-4918-9385-842cfb0ee87a/abri-tu-cuenta-opt.jpg?MOD=AJPERES")';
+document.getElementsByTagName('login')[0].style['background-image'] = 'url("http://www.santander.com.ar/banco/wcm/connect/8adfd3a8-82b0-405d-ab24-8b1363351e07/drakeLogin.png?MOD=AJPERES")';
 
 // Obp
 const swiper = document.querySelector('.carousel-container').swiper;
@@ -29,3 +30,8 @@ const descripcion = document.querySelectorAll('.Home_Header_Noticias_Descripcion
 descripcion.innerHTML = 'Acá te damos un detalle adicional del producto';
 const bckgSlide = document.querySelectorAll('.Home_Swiper_Slide_inner-wrapper.ng-scope')[2];
 bckgSlide.style['background-image'] = 'url("https://www.santander.com.ar/banco/wcm/connect/26012a48-9a72-4231-818d-a86b00680173/Slide_EspecialWomen_Desktop.jpg?MOD=AJPERES")';
+*/
+
+fetch('Output.json')
+  .then(response => response.json())
+  .then(json => console.log(json))
